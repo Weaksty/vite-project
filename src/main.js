9557a725cb2b7b155756560c23582ee1f64d912f
weaksty <rizkoarik9@gmail.com>
2025-07-05 09:53:07 +0200

    я добавил гуи для карточки

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@
 import './style.css'
 import { products } from './products.js'
 //import
+const modul = document.getElementById("modul")
+const closeBth = document.getElementById("close")
+
 
 console.log(products)
 const artk = document.getElementById("Add-Block")
 artk.onclick = function(){
-    window.location.href = "/AddToCataloge/AddToCataloge.html"
+modul.style.display = "flex"   
+}
+closeBth.onclick = function(){
+    modul.style.display = "none"
 }
 
